54fdf22421d1c2a3a9401b8f628c780c17ac2940
Joe Milius <joemilius@gmail.com>
2023-09-12 19:27:48 -0600

    Stats Container created and fetch of players initiated

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import StatsContainer from './components/StatsContainer';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <StatsContainer />
       <StatusBar style="auto" />
     </View>
   );
--- components/StatsContainer.js
+++ components/StatsContainer.js
@@ -1,10 +1,13 @@
-
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function StatsContainer() {
+    fetch(`https://www.balldontlie.io/api/v1/players`)
+    .then(resp => resp.json())
+    .then(data => console.log(data.data))
 
     return(
-        <div>
-            
-        </div>
+        <View>
+            <Text>This is the Stats Container</Text>
+        </View>
     )
 }
