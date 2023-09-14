cb7033a1c685aca3815d397890ba3121acf34da9
Joe Milius <joemilius@gmail.com>
2023-09-13 23:21:10 -0600

    StatsCard created and state established for player stats

---
+++ components/StatsCard.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function StatsCard() {
+
+    return(
+        <View>
+            <Text>This is the Stats Container</Text>
+        </View>
+    )
+}
--- components/StatsContainer.js
+++ components/StatsContainer.js
@@ -1,9 +1,13 @@
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function StatsContainer() {
-    fetch(`https://www.balldontlie.io/api/v1/players`)
-    .then(resp => resp.json())
-    .then(data => console.log(data.data))
+    const [playerStats, setPlaterStats] = useState([])
+    useEffect(() => {
+        fetch(`https://www.balldontlie.io/api/v1/players`)
+        .then(resp => resp.json())
+        .then(data => setPlaterStats(data.data))
+    }, [])
 
     return(
         <View>
