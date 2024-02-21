272c928ccffe4ce610dfb6127c565d542e25004d
Joe Milius <joemilius@gmail.com>
2024-02-20 22:13:01 -0700

    home component created

---
+++ components/Home.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, TextInput, Pressable } from 'react-native';
+
+
+export default function Home(){
+
+
+    return(
+        <View>
+            
+        </View>
+    )
+}
