6b34e2ab10dcbaeb93305f87af886228e79c0235
Joe Milius <joemilius@gmail.com>
2024-03-09 11:05:47 -0700

    rudimentary styling applied to align buttons on the left and data on the right of screen

--- components/Home.js
+++ components/Home.js
@@ -26,59 +26,138 @@ export default function Home(){
         .then(data => setPlayers(data))
         .catch(error => console.log(error))
     }, [])
-    console.log(players[0])
     
 
     function showLists(listType){
-        setListObj({...listObj, [listType]: !listObj[listType]})
+        const newObj = {...listObj}
+        for (key in newObj){
+            if(key === listType){
+                if(newObj[key]===false){
+                    newObj[key] = true  
+                }else{
+                    newObj[key] = false
+                }  
+            }else{
+                newObj[key] = false
+            }
+        }
+        setListObj(newObj)
+        // setListObj({...listObj, [listType]: !listObj[listType]})
     }
 
     const topPoints = [...players].sort((a,b) => b["Points"] - a["Points"])
 
-    const topRebounds = [...players]
+    const topRebounds = [...players].sort((a,b) => b["Rebounds"] - a["Rebounds"])
 
-    const topAssists = [...players]
+    const topAssists = [...players].sort((a,b) => b["Assists"] - a["Assists"])
 
-    const topSteals = [...players]
+    const topSteals = [...players].sort((a,b) => b["Steals"] - a["Steals"])
 
-    const topBlocks = [...players]
+    const topBlocks = [...players].sort((a,b) => b["BlockedShots"] - a["BlockedShots"])
 
     return(
-        <View>
-           
-            <Button
-            title="Top Points"
-            onPress={(e)=> showLists("Top Points")}
-            />
-            {listObj["Top Points"] ?
-            topPoints.slice(0, 10).map(player => {
-                return <Text key={player["PlayerID"]}>{`${player["Name"]}: ${parseInt(player["Points"])} total points`}</Text>
-            })
-            : null}
-            <Button
-            title="Top Rebounds"
-            onPress={(e)=> showLists("Top Rebounds")}
-            />
-            {listObj["Top Rebounds"] ? 
-            <Text>Points</Text>: null}
-            <Button
-            title="Top Assists"
-            onPress={(e)=> showLists("Top Assists")}
-            />
-            {listObj["Top Assists"] ? 
-            <Text>Points</Text>: null}
-            <Button
-            title="Top Steals"
-            onPress={(e)=> showLists("Top Steals")}
-            />
-            {listObj["Top Steals"] ? 
-            <Text>Points</Text>: null}
-            <Button
-            title="Top Blocks"
-            onPress={(e)=> showLists("Top Blocks")}
-            />
-            {listObj["Top Blocks"] ? 
-            <Text>Points</Text>: null}
+        <View style={{flexDirection:'row'}}>
+            <View style={homeStyles.buttonContainer}>
+                <View style={homeStyles.topButtonContainer}>
+                    <Button
+                    title="Top Points"
+                    color={listObj["Top Points"] ? 'red' : 'blue'}
+                    buttonStyle={homeStyles.topButtons}
+                    onPress={(e)=> showLists("Top Points")}
+                    />
+                    <Button
+                    title="Top Rebounds"
+                    color={listObj["Top Rebounds"] ? 'red' : 'blue'}
+                    buttonStyle={homeStyles.topButtons}
+                    onPress={(e)=> showLists("Top Rebounds")}
+                    />
+                    <Button
+                    title="Top Assists"
+                    color={listObj["Top Assists"] ? 'red' : 'blue'}
+                    buttonStyle={homeStyles.topButtons}
+                    onPress={(e)=> showLists("Top Assists")}
+                    />
+                    <Button
+                    title="Top Steals"
+                    color={listObj["Top Steals"] ? 'red' : 'blue'}
+                    buttonStyle={homeStyles.topButtons}
+                    onPress={(e)=> showLists("Top Steals")}
+                    />
+                    <Button
+                    title="Top Blocks"
+                    color={listObj["Top Blocks"] ? 'red' : 'blue'}
+                    buttonStyle={homeStyles.topButtons}
+                    onPress={(e)=> showLists("Top Blocks")}
+                    />
+                </View>
+            </View>
+            <View style={homeStyles.playerList}>
+                <Text>Player List</Text>
+                {listObj["Top Points"] ?
+                topPoints.slice(0, 10).map(player => {
+                    return <Text key={player["PlayerID"]}>{`${player["Name"]}: ${parseInt(player["Points"])} total points`}</Text>
+                })
+                : null}
+                {listObj["Top Rebounds"] ? 
+                topRebounds.slice(0, 10).map(player => {
+                    return <Text key={player["PlayerID"]}>{`${player["Name"]}: ${parseInt(player["Rebounds"])} total rebounds`}</Text>
+                })
+                : null}
+                {listObj["Top Assists"] ? 
+                topAssists.slice(0, 10).map(player => {
+                    return <Text key={player["PlayerID"]}>{`${player["Name"]}: ${parseInt(player["Assists"])} total assists`}</Text>
+                })
+                : null}
+                {listObj["Top Steals"] ? 
+                topSteals.slice(0, 10).map(player => {
+                    return <Text key={player["PlayerID"]}>{`${player["Name"]}: ${parseInt(player["Steals"])} total steals`}</Text>
+                })
+                : null}
+                {listObj["Top Blocks"] ? 
+                topBlocks.slice(0, 10).map(player => {
+                    return <Text key={player["PlayerID"]}>{`${player["Name"]}: ${parseInt(player["BlockedShots"])} total blocks`}</Text>
+                })
+                : null}
+            </View>
         </View>
-    )
+    );
+
+
 }
+const homeStyles = StyleSheet.create({
+    buttonContainer:{
+        flex:1,
+        // display:'flex',
+        marginTop: 20,
+        justifyContent:'flex-start',
+        borderColor:'blue',
+        borderTopWidth:2,
+        borderLeftWidth:2,
+        borderBottomWidth:2,
+        
+    },
+    topButtonContainer:{
+        // display:'flex',
+        flexDirection: 'column',
+        justifyContent:'flex-start',
+        width:100,
+        
+    },
+    topButtons:{
+        textAlign:'center',
+        borderColor:'red',
+        borderWidth:2
+    },
+    playerList:{
+        marginTop:20,
+        width:275,
+        borderColor:'blue',
+        borderWidth:2
+
+        
+
+    },
+    playerListHeader:{
+        marginRight:60
+    }
+})
