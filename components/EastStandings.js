cc18e06cfc4522a15bddc19191433846aace3d04
Joe Milius <joemilius@gmail.com>
2024-04-09 15:04:19 -0600

    west and east components created and conditional rendering set up   Next step create function to switch condition

---
+++ components/EastStandings.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Pressable } from 'react-native';
+
+
+export default function EastStandings(){
+
+    return(
+        <View>
+
+        </View>
+    )
+}
---
+++ components/WestStandings.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Pressable } from 'react-native';
+
+
+export default function WestStandings(){
+
+    return(
+        <View>
+
+        </View>
+    )
+}
