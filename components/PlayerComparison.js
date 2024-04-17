d5f82b9f60edd57392bf1c6b83ad61515a86dfe6
Joe Milius <joemilius@gmail.com>
2024-04-16 23:24:07 -0600

    text inputs for players created and handle function created

--- components/PlayerComparison.js
+++ components/PlayerComparison.js
@@ -4,9 +4,11 @@ import { StyleSheet, Text, View, TextInput, Button, Pressable } from 'react-nati
 
 
     export default function PlayerComparison(){
-        const [playerStats, setPlayerStats] = useEffect([])
-        const [player1, setPlayer1] = useEffect({})
-        const [player2, setPlayer2] = useEffect({})
+        const [playerStats, setPlayerStats] = useState([])
+        const [player1, setPlayer1] = useState({})
+        const [player2, setPlayer2] = useState({})
+        const [search1, setSearch1] = useState('')
+        const [search2, setSearch2] = useState('')
 
         useEffect(() => {
             fetch(`https://api.sportsdata.io/v3/nba/stats/json/PlayerSeasonStats/2024?key=50567f38090849899283c5d9ffd1f55a`,{
@@ -17,8 +19,24 @@ import { StyleSheet, Text, View, TextInput, Button, Pressable } from 'react-nati
             .then(data => setPlayerStats(data))
         }, [])
 
+        function handleInputs(text, inputSrc){
+            if (inputSrc === "player1"){
+                setSearch1(text)
+            }else if (inputSrc === "player2"){
+                setSearch2(text)
+            }
+        }
+
         return(
             <View>
+                <View>
+                    <Text>Player 1</Text>
+                    <TextInput onChangeText={(text) => handleInputs(text, "player1")}/>
+                </View>
+                <View>
+                    <Text>Player 2</Text>
+                    <TextInput onChangeText={(text) => handleInputs(text, "player2")}/>
+                </View>
 
             </View>
         )
